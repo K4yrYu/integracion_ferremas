a7734af5c6e055c853d94b443d0cea33b9e888f1
Felipe <chavezfelipe189@gmail.com>
2025-04-27 01:39:54 -0400

    inicio y registro de sesion

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # cuando entre a /
+    path('iniciosesion/', views.iniciosesion, name='iniciosesion'),  # cuando entre a /iniciosesion/
+    path('registro/', views.registro, name='registro'),
 ]
